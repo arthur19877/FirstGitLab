13212f53dbcb2779c5231db4198de2a541b9e680
Arthur Schvarzburd <arthur19877@gmail.com>
2017-07-03 23:40:19 +0300

    new program

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void print()
+        {
+            string x = "Hello world";
+            Console.WriteLine(x);
+        }
+    }
+}
